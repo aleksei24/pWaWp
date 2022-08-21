fa345b8c1076b95fe0f958be2685d7c00bbaeeed
Aleksei <lexa24032@gmail.com>
2022-08-21 16:01:21 +0300

    needed properties are set in config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,15 @@
 const path = require('path');
+
+module.exports = {
+  mode: 'development',
+
+  entry: './src/index.js',
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'production'),
+    clean: {
+      dry: true,
+    },
+  },
+};
